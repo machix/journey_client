469f247fc2760b695021343759486c0c8b011e01
Jason <nj.ho@hotmail.com>
2018-05-24 19:28:06 -0600

    Color Picker

--- src/Components/Dashes/ContributionDash.js
+++ src/Components/Dashes/ContributionDash.js
@@ -15,11 +15,77 @@ const mapDispatchToProps = dispatch => ({
     onTabClick: (tab) => dispatch({type: 'CHANGE_TAB', tab})
 });
 
+const ContributionButton = props => {
+
+    return (
+        <div className={'contribution-button'}
+             onClick={props.onClick}
+             style={{backgroundColor: props.selected === props.match ? props.selectedColor : null}}>
+            {props.title}
+            <div className={'description'}>
+                {props.description}
+            </div>
+        </div>
+    );
+}
+
+const TransactionInfoTab = props => {
+    const clickHandler = ev => {
+        ev.preventDefault();
+        props.onTabClick('transactions');
+    };
+    return (
+        <li className="tab nav-item">
+            <a
+                href=""
+                className={props.tab === 'transactions' ? 'nav-link is-selected' : 'nav-link'}
+                onClick={clickHandler}>
+                Transaction Information
+            </a>
+        </li>
+    );
+};
+
 
 class ContributionDash extends Component {
 
     constructor(props) {
+
         super(props);
+
+        this.state = {
+            selected: 1
+        }
+    }
+
+    selected = (number) => {
+        console.log(number);
+        switch (number) {
+            case 5:
+                this.setState({
+                    ...this.state,
+                    selected: 5
+                });
+                break;
+            case 10:
+                this.setState({
+                    ...this.state,
+                    selected: 10
+                });
+                break;
+            case 15:
+                this.setState({
+                    ...this.state,
+                    selected: 15
+                });
+                break;
+            case 50:
+                this.setState({
+                    ...this.state,
+                    selected: 50
+                });
+                break;
+        }
     }
 
 
@@ -63,34 +129,35 @@ class ContributionDash extends Component {
                 <div className={'card'}>
 
                     <h2>Select from one of the current needs below</h2>
-                    <div className={'contribution-button'}>
-                        Rations Small: $5
+                    <ContributionButton title={'  Rations Small: $5'}
+                                        description={"We're currently eating beans and canned food for our mainstay. \n \n" +
+                                        "An event will be triggered when we use this."}
+                                        selected={this.state.selected}
+                                        match={5}
+                                        selectedColor={'#c693f0'}
+                                        onClick={() => this.selected(5)}/>
 
-                        <div className={'description'}>
-                            We're currently eating beans and canned food for our mainstay.<br/>
-                            An event will be triggered when we use this.
-                        </div>
-                    </div>
-                    <div className={'contribution-button'}>
-                        Rations Medium: $10
-                        <div className={'description'}>
-                            We're currently eating beans and canned food for our mainstay.<br/>
-                            An event will be triggered when we use this.
-                        </div>
-                    </div>
-                    <div className={'contribution-button'}>
-                        Rations Large: $15
-                        <div className={'description'}>
-                            We're currently eating beans and canned food for our mainstay.<br/>
-                            An event will be triggered when we use this.
-                        </div>
-                    </div>
-                    <div className={'contribution-button'}>
-                        Pickaxe: $50
-                        <div className={'description'}>
-                            Pickaxes are great.
-                        </div>
-                    </div>
+                    <ContributionButton title={'  Rations Medium: $10'}
+                                        description={"We're currently eating beans and canned food for our mainstay. \n \n" +
+                                        "An event will be triggered when we use this."}
+                                        selected={this.state.selected}
+                                        match={10}
+                                        selectedColor={'#899cf0'}
+                                        onClick={() => this.selected(10)}/>
+
+                    <ContributionButton title={'  Rations Small: $15'}
+                                        description={"We're currently eating beans and canned food for our mainstay. \n \n" +
+                                        "An event will be triggered when we use this."}
+                                        selected={this.state.selected}
+                                        match={15}
+                                        selectedColor={'#f08159'}
+                                        onClick={() => this.selected(15)}/>
+                    <ContributionButton title={'                        Pickaxe: $50\n'}
+                                        description={'Pickaxes are great.'}
+                                        selected={this.state.selected}
+                                        match={50}
+                                        selectedColor={'#3ac732'}
+                                        onClick={() => this.selected(50)}/>
 
                 </div>
                 <div className={'card'}>
